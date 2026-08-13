d78bb83a7ca8a8911d9ef74b70d1acf4254e6a9d
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2026-08-13 23:46:06 +0930

    feat(panel): expose separated-control API

---
+++ src/qec/routing/panel/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: MPL-2.0
+"""Panel separated-control routing API."""
+from .core import PanelExchange, PanelFaultPlan, PanelRequest, PanelTopology, TranslationTable, build_claim_validation, build_fault_battery, validate_route_receipt
